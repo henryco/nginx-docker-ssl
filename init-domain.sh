4cb3e2919cf1beedd6bf4c32a901410d775e899e
IrlHenry <henrycodev@gmail.com>
2019-05-12 10:15:56 +0200

    Update init-domain.sh

--- init-domain.sh
+++ init-domain.sh
@@ -3,6 +3,7 @@
 DOMAIN=$1
 EMAIL=$2
 FNAME="$DOMAIN.conf"
+SNAME="www.$FNAME"
 CUR=$PWD
 
 cd data/nginx/
@@ -44,6 +45,36 @@ echo "        proxy_set_header    X-Real-IP           \$remote_addr;" >> $FNAME
 echo "        proxy_set_header    X-Forwarded-For     \$proxy_add_x_forwarded_for;" >> $FNAME
 echo "    }" >> $FNAME
 echo "}" >> $FNAME
+
+
+echo "server {" > $SNAME
+echo "    listen 80;" >> $SNAME
+echo "    server_name www.$DOMAIN;" >> $SNAME
+echo "    server_tokens off;" >> $SNAME
+echo "" >> $SNAME
+echo "    root /usr/share/nginx/html;" >> $SNAME
+echo "" >> $SNAME
+echo "    location /.well-known/acme-challenge/ {" >> $SNAME
+echo "        root /var/www/certbot;" >> $SNAME
+echo "    }" >> $SNAME
+echo "}" >> $SNAME
+echo "server {" >> $SNAME
+echo "    listen 443 ssl;" >> $SNAME
+echo "    server_name www.$DOMAIN;" >> $SNAME
+echo "    server_tokens off;" >> $SNAME
+echo "" >> $SNAME
+echo "    ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;" >> $SNAME
+echo "    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;" >> $SNAME
+echo "    include /etc/letsencrypt/options-ssl-nginx.conf;" >> $SNAME
+echo "    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;" >> $SNAME
+echo "" >> $SNAME
+echo "    location / {" >> $SNAME
+echo "        proxy_pass http://www.$DOMAIN;" >> $SNAME
+echo "        proxy_set_header    Host                \$http_host;" >> $SNAME
+echo "        proxy_set_header    X-Real-IP           \$remote_addr;" >> $SNAME
+echo "        proxy_set_header    X-Forwarded-For     \$proxy_add_x_forwarded_for;" >> $SNAME
+echo "    }" >> $SNAME
+echo "}" >> $SNAME
 
 cd $CUR
 
